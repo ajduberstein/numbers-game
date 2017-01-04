ca381d69ccfe064b7006f1bf0f07f1b5c31dd51e
Andrew Duberstein <duberstein@uber.com>
2017-01-03 18:08:27 -0800

    Add one test

--- src/client/app/App-spec.js
+++ src/client/app/App-spec.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+
+import gameReducer from './Game/GameReducers';
 import App from './App';
 
+const rootReducer = gameReducer;
+
+const store = createStore(
+    rootReducer
+);
+
+
 describe('App (Snapshot)', () => {
-  it('Snapshot renders', () => {
-    const component = renderer.create(<App />);
+  it('App renders', () => {
+      const component = renderer.create(
+          <Provider store={store}><App /></Provider>
+      );
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
   });

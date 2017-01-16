aebcde97c0759de7bba222572fd95833c94abdc5
Andrew Duberstein <duberstein@uber.com>
2017-01-16 14:03:27 -0800

    Add container test

--- src/client/app/Game/GameContainer-spec.js
+++ src/client/app/Game/GameContainer-spec.js
@@ -1,14 +1,23 @@
-import * as sinon from 'Sinon';
-
-import Game from './Display/Game';
+import { Provider } from 'react-redux'
+import GameContainer from './GameContainer';
 import * as actions from './GameActions';
 import * as redux from 'react-redux';
-
+import React from 'react';
+import configureStore from 'redux-mock-store'
 import {shallow} from 'enzyme';
+
+const middlewares = []
+const mockStore = configureStore(middlewares)
+
+
 
 describe('connect', () => {
     it('connect actions to the store', () => {
-        const component = shallow(<Game />);
+        const component = shallow(
+            <Provider store={store}>
+              <GameContainer />
+            </Provider>
+        );
         expect(redux.connect).toHaveBeenCalled();
     });
 });
